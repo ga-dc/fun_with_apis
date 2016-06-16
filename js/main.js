9ac3485514e413e399667247f437a9e874201e75
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2016-06-16 14:54:04 -0400

    Add Handlebars template to display articles

--- js/main.js
+++ js/main.js
@@ -1,40 +1,71 @@
-// initial API call
-$( document ).ready( function() {
-  $( "#search-form button" ).on( 'click', function(){
+'use strict';
 
-    // Obtein user inputted search term and location.
-    var searchTerm   = $( 'input[name="searchTerm"]' ).val();
+(function(){
 
-    console.log(searchTerm);
+  /**
+   * Handle form submission and initiate an API call via ajax.
+   */
+  $( document ).ready( function() {
 
-    // Build an API query URL.
-    var baseUrl = [
-      'https://api.nytimes.com/svc/search/v2/articlesearch.json',
-      '?api-key=d5d517dae8224de0b5a659a4b5d01ce7'
-    ].join('');
+    // If the ENTER key was clicked on the form,
+    // trigger the click on the search button.
+    $( "#search-form" ).on( 'keypress', function( event ){
 
-    // Build a query url based on user's input.
-    var queryUrl = baseUrl + '&q=' + searchTerm;
+      if ( event && event.keyCode === 13 ) {
+        $( "#search-form button" ).trigger( "click" );
+        return false; // NOTE: Stop the event.
+      }
+    });
 
-    searchTerm = "";
+    $( "#search-form button" ).on( 'click', function(){
 
-    $.ajax({
-      url: queryUrl,
-      method: 'GET',
-    }).done(function(data) {
-      console.log(data);
+      // Obtein user inputted search term and location.
+      var searchTerm   = $( 'input[name="searchTerm"]' ).val();
 
-      var articles = data.response.docs;
-      var article  = articles[0];
+      console.log( "searchTerm: %s", searchTerm );
 
-      $( '.lead_paragraph' ).text( article.lead_paragraph );
-      $( '.abstract' ).text( article.abstract );
-      $( '.pub_date' ).text( article.pub_date );
-      $( '.web_url' ).text( article.web_url );
+      // Build an API query URL.
+      var baseUrl = [
+        'https://api.nytimes.com/svc/search/v2/articlesearch.json',
+        '?api-key=d5d517dae8224de0b5a659a4b5d01ce7'
+      ].join('');
 
-    }).fail(function(err) {
-      console.log(err);
-      throw err;
+      // Build a query url based on user's input.
+      var queryUrl = baseUrl + '&q=' + searchTerm + '&sort=newest';
+
+      searchTerm = "";
+
+      $.ajax({
+        url: queryUrl,
+        method: 'GET',
+      }).done( function( data ) {
+        console.log( data );
+
+        var articles = data.response.docs;
+        dataToHtml( articles )
+
+      }).fail( function( err ) {
+        console.log( err );
+        throw err;
+      });
     });
   });
-});
+
+  /**
+   * Render HTML based on the specified data and the template defined above.
+   * @param  {Array<Object>} articles
+   */
+  function dataToHtml( articles ) {
+    // Get reference to the Characters template source.
+    var source = document.querySelector( '#articles-template' ).innerHTML;
+
+    // Compile the template.
+    var template = Handlebars.compile( source );
+
+    // Pass the articles data into the template as articles.
+    var output = template({ articles: articles });
+
+    // Insert the template into the target element.
+    document.querySelector( '.articles' ).innerHTML = output;
+  }
+})();
